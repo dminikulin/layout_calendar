270f5cb90a77f9daaf4f6a0711130f670ee6a0ba
Your Name <your@mail.com>
2025-07-21 13:14:51 +0300

    Message

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -3,5 +3,8 @@ module.exports = {
   plugins: [
     "stylelint-scss"
   ],
-  rules: {}
+  rules: {
+    "scss/operator-no-newline-after": null,
+  "scss/dollar-variable-empty-line-before": null
+  }
 };
